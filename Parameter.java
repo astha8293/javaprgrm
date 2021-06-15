d0f33825fadc29181471bfacb11914dd2fa3c2a0
astha8293 <astha.pandya12@gmail.com>
2021-06-15 10:25:50 +0530

    second

---
+++ Parameter.java
@@ -0,0 +1,17 @@
+public class Parameter{
+	
+	static void solve(int a,int b){
+		System.out.println("R1 = " +(a+b));
+		System.out.println("R2 = " +(a-b));
+		System.out.println("R3 = " +(a*b));
+		System.out.println("R4 = " +(a/b));
+		}
+	public static void main(String[] args){
+		solve(20,8);
+	
+		
+		
+		
+	}
+	
+}
